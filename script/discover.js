343a5d265ebdd9d526dedd7d6a231925adcd9725
Ireese <ireese@linz.govt.nz>
2020-10-31 15:40:56 +1300

    vector tile connection

--- script/discover.js
+++ script/discover.js
@@ -10,6 +10,69 @@ function insert() {
     $('.menu').append('<div class="menuitem"><a href="connect.html">Connect</a></div>');
     $('.menu').append('<div class="menuitem"><a href="about.html">About</a></div>');
 
+    //Begin vector tiles
+
+//Vector Tiles: Available
+
+//layer url
+var urlVectorAvailable = "https://xycarto.github.io/vectortile-repo/available_now/{z}/{x}/{y}.pbf";
+
+var stylesStartAvailable = {
+    interactive: true,
+    getFeatureID: function(f) {
+      return f.layer.properties;
+    },
+    vectorTileLayerStyles: {
+      'LiDAR_available_now': function(properties,zoom) {
+          var level = map.getZoom();
+          var weight = 0.5;
+          if (level >= 8) {weight = 1.5;}
+          return {
+            weight: weight,
+            opacity: 1,
+            color: "#d677d6",
+            fillColor: "#e4a5e4",
+            fillOpacity: 0.75,
+            fill: true
+          }
+    }}
+};
+
+//add functionality to vector layer
+var vectorAvailable = L.vectorGrid.protobuf(urlVectorAvailable, stylesStartAvailable)
+.on('click', function(e) {
+var popupName = '<h3 style="text-align: center;">' + e.layer.properties.name + '</h3>';
+var popupDensity = '<div id="popUpText"><strong>Point Density: </strong>' + e.layer.properties.point_dens + '</div>';
+var popupVertical = '<div id="popUpText"><strong>Vertical Datum: </strong>' + e.layer.properties.vertical_d + '</div>';
+var popupHorizontal = '<div id="popUpText"><strong>Horizontal Datum: </strong>' + e.layer.properties.horizontal + '</div>';
+var popupSupplier = '<div id="popUpText"><strong>Supplier: </strong>' + e.layer.properties.supplier + '</div>';
+var popupFlownFrom = '<div id="popUpText"><strong>Flown From: </strong>' + e.layer.properties.flown_from + '</div>';
+var popupFlownTo = '<div id="popUpText"><strong>Flown To: </strong>' + e.layer.properties.flown_to + '</div>';
+var popupGetData = '<div id="popUpText"><strong>Get Data: </strong>' + '<ul><li><a href="' + e.layer.properties.DataDEM + '" target="_blank">Digital Elevation Model(DEM)</a></li><li><a href="'+ e.layer.properties.DataDSM + '" target="_blank">Digital Surface Model(DSM)</a></li><li><a href="' + e.layer.properties.DataPointC+ '" target="_blank">Point Cloud(LAS)</a></li></ul></div>';
+$("#sp-head").text(e.layer.properties.name);
+$("#fill-items").text(e.layer.properties.point_dens, e.layer.properties.vertical_d);
+    L.popup()
+      .setContent(popupName + popupDensity + popupVertical + popupHorizontal + popupSupplier + popupFlownFrom + popupFlownTo+ popupGetData)
+      .setLatLng(e.latlng)
+      .openOn(map);
+})
+.on("mouseover", function(e) {
+  e.layer.setStyle({
+    fillColor: "yellow"
+  })
+})
+.on("mouseout", function(e) {
+  e.layer.setStyle({
+    //weight: weight,
+    opacity: 1,
+    color: "#d677d6",
+    fillColor: "#e4a5e4",
+    fillOpacity: 0.75,
+    fill: true
+  });
+});
+
+
 
 //Build Basemap    
     var settingsBasemap = {
@@ -30,7 +93,7 @@ function insert() {
     };
 
     var overlays = {
-        //"Available Now": vectorAvailable,
+        "Available Now": vectorAvailable,
         //"Coming Soon": vectorComingSoon,
         //"In Progress": vectorInProgress
       };
