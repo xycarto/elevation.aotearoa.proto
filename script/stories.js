38cea6e6469dd9090121f1b4419e8cd46b8f3d32
Ireese <ireese@linz.govt.nz>
2020-10-31 14:27:49 +1300

    add stories js page, fix js link

---
+++ script/stories.js
@@ -0,0 +1,17 @@
+
+function insert() { 
+    $('body').append('<div class="top"><a href="index.html">Elevation Aotearoa</a></div>');
+    $('body').append('<div class="menu"></div>');
+    $('body').append('<div class="landboxframe">Stories Page Here</div>');
+    $('.menu').append('<div class="menuitem"><a href="discover.html">Discover Data</a></div>');
+    $('.menu').append('<div class="menuitem"><a href="stories.html">Our Stories</a></div>');
+    $('.menu').append('<div class="menuitem"><a href="learn.html">Learn</a></div>');
+    $('.menu').append('<div class="menuitem"><a href="connect.html">Connect</a></div>');
+    $('.menu').append('<div class="menuitem"><a href="about.html">About</a></div>');
+};
+
+
+
+
+    
+
