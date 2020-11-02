749d0f133c79724998294440efd7baf3d3106809
Ireese <ireese@linz.govt.nz>
2020-11-02 15:31:36 +1300

    layout changes

--- script/main.js
+++ script/main.js
@@ -1,11 +1,11 @@
 
 function insert() { 
     $('body').append('<div class="top"></div>');
-        $('.top').append('<div class="menuitem-index"><a href="index.html">Elevation Aotearoa</a></div>');
-        $('.top').append('<div class="menuitem"><span><a href="discover.html">Discover Data</a></span></div>');
-        $('.top').append('<div class="menuitem"><a href="learn.html">Learn</a></div>');
-        $('.top').append('<div class="menuitem"><a href="connect.html">Connect</a></div>');
-        $('.top').append('<div class="menuitem"><a href="about.html">About</a></div>');
+        $('.top').append('<div class="menuitem-index"><a href="index.html"><span>Elevation Aotearoa</span></a></div>');
+        $('.top').append('<div class="menuitem"><a href="discover.html"><span>Discover Data</span></a></div>');
+        $('.top').append('<div class="menuitem"><span><a href="learn.html">Learn</a></span></div>');
+        $('.top').append('<div class="menuitem"><span><a href="connect.html">Connect</a></span></div>');
+        $('.top').append('<div class="menuitem"><span><a href="about.html">About</a></span></div>');
 
 
     $('body').append('<div class="imageholder"></div>');
